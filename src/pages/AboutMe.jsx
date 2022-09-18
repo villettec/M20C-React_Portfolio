a62aa38884008b3649226a54be3b81b4728a9297
Villette Comfort <villette@live.com>
2022-09-18 15:37:35 -0700

    Testing deployment

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -6,13 +6,23 @@ const AboutMe = () => {
   return (
     <div>
         <Header/>
-        <h1> About Me</h1>
-        <p>Villette is currently enrolled in a full-stack development bootcamp at the University of Arizona. Thus far she has learned HTML, CSS, JavaScript, jQuery, Bootstrap, and Node.js. In the future, she will be learning MySQL, MongDB, Express.js, and React.js. She hopes to find a career that will utilize these skills.
 
+        <section classname="about-section">
+            <h2>About</h2>
+            <article>
+                <p>
+                    Villette is currently enrolled in a full-stack development bootcamp at the University of Arizona. Thus far she has learned HTML, CSS, JavaScript, jQuery, Bootstrap, and Node.js. In the future, she will be learning MySQL, MongDB, Express.js, and React.js. She hopes to find a career that will utilize these skills.
+                </p>
+                <p>
+                    Previous to pursuing a career in software development, Villette has been a teacher. Her Bachelor's degree is in English, and Post-Baccalaureate is in Secondary Education. She has primarily taught high school with a focus on advanced literature but is also certified as an art teacher.
+                </p>
+            </article>
+        </section>
 
-Previous to pursuing a career in software development, Villette has been a teacher. Her Bachelor's degree is in English, and Post-Baccalaureate is in Secondary Education. She has primarily taught high school with a focus on advanced literature but is also certified as an art teacher.</p>
         <Footer/>
-    </div>
+    
+    </div >
+
   )
 }
 
