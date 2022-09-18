de1726db0e15c51d0bbd3391ae5e92c0e31dc51c
Villette Comfort <villette@live.com>
2022-09-18 15:23:02 -0700

    Added about me text

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -7,7 +7,10 @@ const AboutMe = () => {
     <div>
         <Header/>
         <h1> About Me</h1>
-        <p>About me writing goes here</p>
+        <p>Villette is currently enrolled in a full-stack development bootcamp at the University of Arizona. Thus far she has learned HTML, CSS, JavaScript, jQuery, Bootstrap, and Node.js. In the future, she will be learning MySQL, MongDB, Express.js, and React.js. She hopes to find a career that will utilize these skills.
+
+
+Previous to pursuing a career in software development, Villette has been a teacher. Her Bachelor's degree is in English, and Post-Baccalaureate is in Secondary Education. She has primarily taught high school with a focus on advanced literature but is also certified as an art teacher.</p>
         <Footer/>
     </div>
   )
