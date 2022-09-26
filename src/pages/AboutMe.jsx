7d1a0590c85f76a9befbe37556591a3fadc5cc76
violet978 <villette@live.com>
2022-09-25 21:43:29 -0700

    Added readme info

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -9,10 +9,10 @@ const AboutMe = () => {
             <h2>About</h2>
             <article>
                 <p>
-                    Villette is currently enrolled in a full-stack development bootcamp at the University of Arizona. Thus far she has learned HTML, CSS, JavaScript, jQuery, Bootstrap, and Node.js. In the future, she will be learning MySQL, MongDB, Express.js, and React.js. She hopes to find a career that will utilize these skills.
+                    Villette is currently completing a full-stack development bootcamp at the University of Arizona. She has learned HTML, CSS, JavaScript, jQuery, Bootstrap, and Node.js, MySQL, MongDB, Express.js, and React.js. She hopes to find a career that will utilize these skills.
                 </p>
                 <p>
-                    Previous to pursuing a career in software development, Villette has been a teacher. Her Bachelor's degree is in English, and Post-Baccalaureate is in Secondary Education. She has primarily taught high school with a focus on advanced literature but is also certified as an art teacher.
+                    Previous to pursuing a career in software development, Villette has been a teacher. Her Bachelor's degree is in English, and Post-Baccalaureate is in Secondary Education. She has primarily taught high school with a focus on advanced literature with additional certifications in art.
                 </p>
             </article>
         </section>
