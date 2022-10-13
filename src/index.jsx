48c2c868ea1321e44405337ceac49229c7e98708
violet978 <villette@live.com>
2022-10-12 17:12:01 -0700

    Imported bootstrap which required larger background image

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faCoffee} from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
