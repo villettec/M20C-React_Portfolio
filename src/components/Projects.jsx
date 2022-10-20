38b8371f3617f0f47c23014fec3e09944a000686
violet978 <villette@live.com>
2022-10-19 23:14:55 -0700

    Rearranged projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,17 +14,16 @@ const Projects = () => {
       <div className="project-description">
         <h3>Weather Dashboard</h3>
         <p>In this application, a user searches a city and both the current and 5 day forecast
-          information is displayed by calling an API for data.<a href="https://github.com/villettec/M6C-Weather_Dashboard"> Repo.</a></p>
+          information is displayed by calling an API for data.<a href="https://github.com/villettec/M6C-Weather_Dashboard" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
-      <a href="https://villettec.github.io/M3C-Password_Generator/" target="_blank" rel="noopener noreferrer">
-        <img src="../../images/pwgen-pic.png" alt="Password Generator Screenshot" className="capture" />
+      <a href="https://villettec.github.io/School_Resources/" target="_blank" rel="noopener noreferrer">
+        <img src="../../images/school-resources-pic.png" alt="School Resource Library Screenshot" className="capture" />
       </a>
       <div className="project-description">
-        <h3>Password Generator</h3>
-        <p>Via prompts, a user chooses whether to include symbols, numbers, or letters in a generated
-          password of their choice of length.<a href="https://github.com/villettec/M3C-Password_Generator"> Repo.</a></p>
+        <h3>School Resource Library</h3>
+        <p>This full stack JavaScript website uses 3 API's to enable students to view school library books based on subject.<a href="https://github.com/villettec/School_Resources" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -34,7 +33,26 @@ const Projects = () => {
       <div className="project-description">
         <h3>Coding Quiz</h3>
         <p>This application is a timed quiz displaying array questions and answers. Ordered high scores
-          and saved initials are featured at the end.<a href="https://github.com/villettec/M4C-Code_Quiz"> Repo.</a></p>
+          and saved initials are featured at the end.<a href="https://github.com/villettec/M4C-Code_Quiz" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
+      </div>
+    </div>
+    <div className="project-container">
+      <a href="https://gator-tracker-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+        <img src="../../images/gator-tracker-pic.png" alt="Gator Tracker Screenshot" className="capture" />
+      </a>
+      <div className="project-description">
+        <h3>Gator Tracker</h3>
+        <p>This collaborative full-stack project allows users to post local neighborhood alligator sightings in the southeast United States.<a href="https://github.com/villettec/Gator_Aid" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
+      </div>
+    </div>
+    <div className="project-container">
+      <a href="https://villettec.github.io/M3C-Password_Generator/" target="_blank" rel="noopener noreferrer">
+        <img src="../../images/pwgen-pic.png" alt="Password Generator Screenshot" className="capture" />
+      </a>
+      <div className="project-description">
+        <h3>Password Generator</h3>
+        <p>Via prompts, a user chooses whether to include symbols, numbers, or letters in a generated
+          password of their choice of length.<a href="https://github.com/villettec/M3C-Password_Generator" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -44,7 +62,7 @@ const Projects = () => {
       <div className="project-description">
         <h3>Workday Scheduler</h3>
         <p>In this day planner, moments and jquery are used to show events of the past, present, and
-          future which the user can enter and erase.<a href="https://github.com/villettec/M5C-Work_Day_Scheduler"> Repo.</a></p>
+          future which the user can enter and erase.<a href="https://github.com/villettec/M5C-Work_Day_Scheduler" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -54,7 +72,7 @@ const Projects = () => {
       <div className="project-description">
         <h3>Horiseon Refactor</h3>
         <p>This was an exercise in code refactoring to make the code apply to accessibility standards
-          and SEO optimization.<a href="https://github.com/villettec/M1C-Horiseon_Refactor/"> Repo.</a></p>
+          and SEO optimization.<a href="https://github.com/villettec/M1C-Horiseon_Refactor/" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -64,7 +82,7 @@ const Projects = () => {
       <div className="project-description">
         <h3>To Do List Maker</h3>
         <p>This To Do List application allows a user to input tasks and mark them as completed which
-          strikes them out or later deletes them. Tasks can be filtered by completion.<a href="https://github.com/villettec/JS-To_Do_List/"> Repo.</a></p>
+          strikes them out or later deletes them. Tasks can be filtered by completion.<a href="https://github.com/villettec/JS-To_Do_List/" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -74,7 +92,7 @@ const Projects = () => {
       <div className="project-description">
         <h3>README Generator</h3>
         <p>This is a node application which prompts the user with input questions and generates a
-          README. A walkthrough video is above.<a href="https://github.com/villettec/M9C-Readme_Generator"> Repo</a>.</p>
+          README. A walkthrough video is above.<a href="https://github.com/villettec/M9C-Readme_Generator" target="_blank" rel="noopener noreferrer"> Repo</a>.</p>
       </div>
     </div>
     <div className="project-container">
@@ -84,7 +102,7 @@ const Projects = () => {
       <div className="project-description">
         <h3>Team Generator</h3>
         <p>This node application prompts the user with questions about employees and generates a HTML
-          and CSS page. A walkthrough video is above. <a href="https://github.com/villettec/M10C-Team_Profile_Generator"> Repo</a>.</p>
+          and CSS page. A walkthrough video is above. <a href="https://github.com/villettec/M10C-Team_Profile_Generator" target="_blank" rel="noopener noreferrer"> Repo</a>.</p>
       </div>
     </div>
     <div className="project-container">
@@ -93,7 +111,7 @@ const Projects = () => {
       </a>
       <div className="project-description">
         <h3>Note Taker</h3>
-        <p>This full stack express application allows the user to write and save notes which uses the express server to write and retrieve them from a database file.<a href="https://github.com/villettec/M11C-Note_Taker"> Repo.</a></p>
+        <p>This full stack express application allows the user to write and save notes which uses the express server to write and retrieve them from a database file.<a href="https://github.com/villettec/M11C-Note_Taker" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -103,7 +121,7 @@ const Projects = () => {
       <div className="project-description">
         <h3>Employee Tracker</h3>
         <div className="p-div">
-        <p>This is a MySQl application that allows users to update the database via node prompts. A walkthrough video is above.<a href="https://github.com/villettec/M12C-Employee_Tracker"> Repo.</a></p>
+        <p>This is a MySQl application that allows users to update the database via node prompts. A walkthrough video is above.<a href="https://github.com/villettec/M12C-Employee_Tracker" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
         </div>
       </div>
     </div>
@@ -113,7 +131,7 @@ const Projects = () => {
       </a>
       <div className="project-description">
         <h3>E-Commerce Site Back-End</h3>
-        <p>An Express.js API is configured to use Sequelize to interact with a MySQL database. A walkthrough video is above.<a href="https://github.com/villettec/M13C-E-commerce_Back_End"> Repo.</a></p>
+        <p>An Express.js API is configured to use Sequelize to interact with a MySQL database. A walkthrough video is above.<a href="https://github.com/villettec/M13C-E-commerce_Back_End" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -122,16 +140,7 @@ const Projects = () => {
       </a>
       <div className="project-description">
         <h3>Tech Blog</h3>
-        <p>This full-stack website allows a user to login and post new blog posts or comment on them.<a href="https://github.com/villettec/M14C-Tech_Blog"> Repo.</a></p>
-      </div>
-    </div>
-    <div className="project-container">
-      <a href="https://gator-tracker-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-        <img src="../../images/gator-tracker-pic.png" alt="Gator Tracker Screenshot" className="capture" />
-      </a>
-      <div className="project-description">
-        <h3>Gator Tracker</h3>
-        <p>This collaborative full-stack project allows users to post local neighborhood alligator sightings in the southeast United States.<a href="https://github.com/villettec/Gator_Aid"> Repo.</a></p>
+        <p>This full-stack website allows a user to login and post new blog posts or comment on them.<a href="https://github.com/villettec/M14C-Tech_Blog" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -140,7 +149,7 @@ const Projects = () => {
       </a>
       <div className="project-description">
         <h3>Social Network API</h3>
-        <p>This social network api allows users to post/edit/delete thoughts, reactions, and friends using Express.js for routing, MongoDB database, and Mongoose ODM.<a href="https://github.com/villettec/M18C-Social_Network_API"> Repo.</a></p>
+        <p>This social network api allows users to post/edit/delete thoughts, reactions, and friends using Express.js for routing, MongoDB database, and Mongoose ODM.<a href="https://github.com/villettec/M18C-Social_Network_API" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
@@ -149,16 +158,7 @@ const Projects = () => {
       </a>
       <div className="project-description">
         <h3>Moodify</h3>
-        <p>This collaborative MERN full stack project allows authenticated users to login and generate music playlists based on the Spotify API.<a href="https://github.com/batgal/Moodify"> Repo.</a></p>
-      </div>
-    </div>
-    <div className="project-container">
-      <a href="https://villettec.github.io/School_Resources/" target="_blank" rel="noopener noreferrer">
-        <img src="../../images/school-resources-pic.png" alt="School Resource Library Screenshot" className="capture" />
-      </a>
-      <div className="project-description">
-        <h3>School Resource Library</h3>
-        <p>This full stack JavaScript website uses 3 API's to enable students to view school library books based on subject.<a href="https://github.com/villettec/School_Resources"> Repo.</a></p>
+        <p>This collaborative MERN full stack project allows authenticated users to login and generate music playlists based on the Spotify API.<a href="https://github.com/batgal/Moodify" target="_blank" rel="noopener noreferrer"> Repo.</a></p>
       </div>
     </div>
     <div className="project-container">
