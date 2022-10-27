3736258c206b15fbe37fa1db73f4e85797f415a8
violet978 <villette@live.com>
2022-10-26 21:30:09 -0700

    Updated about

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -13,7 +13,7 @@ const AboutMe = () => {
             <h2>About</h2>
             <article>
                 <p>
-                    Villette is a full stack developer with a passion for front end design. Having initially taught herself to code she attended University of Arizona's full stack web development certification program to enhance her skills. Her projects include tech stacks such as React.js, JavaScript, HTML, CSS, Bootstrap, Bulma, Node.js, MySQL, MongoDB, Express.js, and graphic design. She hopes to find a career that will utilize these skills.
+                    Villette is a full stack developer with a passion for front end design. Having initially taught herself to code she attended University of Arizona's full stack web development certification program to enhance her skills. Her projects include technology such as React.js, JavaScript, HTML, CSS, Bootstrap, Node.js, MySQL, MongoDB, Express.js, and graphic design. She hopes to find a career that will utilize these skills.
                 </p>
                 <p>
                     In her previous role, Villette has been a teacher. Her Bachelor's degree is in English, and Post-Baccalaureate is in Secondary Education. She has primarily taught high school with a focus on advanced literature with additional certifications in art.
